5dd6726e4d9a1961803f9cd88fe956421fdcdd0d
Pranith Rao <pranithrao3@gmail.com>
2024-10-10 20:57:59 +0530

    Added pages & completed home page

--- app.py
+++ app.py
@@ -12,5 +12,25 @@ db = SQLAlchemy(app)
 def home():
     return render_template("index.html")
 
+@app.route("/crisisDashboard")
+def crisisDashboard():
+    return render_template("crisisDashboard.html")
+
+@app.route("/requestResources")
+def requestResources():
+    return render_template("requestResources.html")
+
+@app.route("/donateSupport")
+def donateSupport():
+    return render_template("donateSupport.html")
+
+@app.route("/volunteerSignup")
+def volunteerSignup():
+    return render_template("volunteerSignup.html")
+	
+@app.route("/supplyManagement")
+def supplyManagement():
+    return render_template("supplyManagement.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
